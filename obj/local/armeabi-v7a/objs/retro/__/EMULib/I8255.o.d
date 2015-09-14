6ca41ca81fdacb575a6e9e11612f0d758bdcbd84
Franco Catrin <fcatrin@gmail.com>
2015-09-14 00:57:08 +0000

    update build system

--- obj/local/armeabi-v7a/objs/retro/__/EMULib/I8255.o.d
+++
@@ -1,5 +0,0 @@
-/Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/obj/local/armeabi-v7a/objs/retro/__/EMULib/I8255.o: \
- /Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/I8255.c \
- /Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/I8255.h
-
-/Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/I8255.h:
--- obj/local/armeabi-v7a/objs/retro/__/EMULib/SHA1.o.d
+++
@@ -1,5 +0,0 @@
-/Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/obj/local/armeabi-v7a/objs/retro/__/EMULib/SHA1.o: \
- /Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/SHA1.c \
- /Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/SHA1.h
-
-/Users/fcatrin/tmp/retrobox/retroarch-1.2.2/libretro-super/libretro-fmsx/jni/../EMULib/SHA1.h:
